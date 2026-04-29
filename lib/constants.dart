084678ae5479dd7fb3019cee0c865fa927d58090
Yotam Aviv <yotam@aviv.net>
2026-04-29 07:52:27 -0700

    deleting all dart code

--- lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:oneofus_common/trust_statement.dart' show kOneofusDomain;
-export 'package:oneofus_common/trust_statement.dart' show kOneofusDomain;
-
-const String kHablotengo = 'hablotengo.com';
-const String kHabloContactCollection = 'hablotengo_contact';
-const String kHabloPrivacyCollection = 'hablotengo_privacy';
-
-const String kHabloSigninUrl = 'https://signin.hablotengo.com/signin';
--- lib/fire_choice.dart
+++
@@ -1,9 +0,0 @@
-enum FireChoice {
-  fake,
-  emulator,
-  prod;
-}
-
-FireChoice fireChoice = const String.fromEnvironment('fire') == 'emulator'
-    ? FireChoice.emulator
-    : FireChoice.prod;
